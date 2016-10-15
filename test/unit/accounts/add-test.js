bd11905a56d31e6554a429a482609213e242cbbb
Gregor <gregor@martynus.net>
2016-10-15 13:49:23 -0700

    test: unit tests for accounts.add
    prepared test for https://github.com/hoodiehq/camp/issues/60

---
+++ test/unit/accounts/add-test.js
@@ -0,0 +1,32 @@
+var test = require('tap').test
+
+var addAccount = require('../../../lib/accounts/add')
+
+test('addAccount', function (group) {
+  group.test('with empty username', {skip: true}, function (t) {
+    t.plan(3)
+
+    var state = {
+      db: {
+        put: function () {
+          throw new Error('should not call db.put')
+        }
+      }
+    }
+    addAccount(state, {
+      username: ''
+    })
+
+    .then(function () {
+      t.fail('addAccount should reject')
+    })
+
+    .catch(function (error) {
+      t.is(error.name, 'Bad Request')
+      t.is(error.status, 400)
+      t.is(error.message, 'username must be set')
+    })
+  })
+
+  group.end()
+})
