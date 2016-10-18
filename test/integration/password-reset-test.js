b668a9eaaf92b9832bc009e40be8011d55912a2d
Gregor <gregor@martynus.net>
2016-10-17 19:11:09 -0700

    test: invalidate token on sessions.add and accounts.update

--- test/integration/password-reset-test.js
+++ test/integration/password-reset-test.js
@@ -6,7 +6,6 @@ var test = require('tap').test
 var getApi = require('../../')
 
 test('password reset', function (t) {
-  var tokenId
   var api = getApi({
     PouchDB: PouchDB,
     secret: 'secret'
@@ -22,28 +21,36 @@ test('password reset', function (t) {
     t.is(account.username, 'foo', 'creates account')
 
     return api.account({id: 'user123'}).tokens.add({
+      id: 'secrettoken',
       type: 'passwordreset'
     })
   })
 
   .then(function (token) {
-    tokenId = token.id
-    t.ok(token.id, 'token created')
+    t.is(token.id, 'secrettoken', 'token created')
 
-    return api.sessions.add({
-      account: {
-        token: token.id
-      }
+    return api.accounts.find({
+      token: 'secrettoken'
     })
   })
 
-  .then(function (session) {
-    t.ok(session.account.id, 'session created')
+  .then(function (account) {
+    t.is(account.username, 'foo', 'finds account by token')
 
-    return api.sessions.add({
-      account: {
-        token: tokenId
-      }
+    return api.accounts.update({
+      token: 'secrettoken'
+    }, {
+      password: 'newsecret'
+    })
+  })
+
+  .then(function (account) {
+    t.pass('password updated')
+
+    return api.accounts.update({
+      token: 'secrettoken'
+    }, {
+      password: 'yetnewsecret'
     })
 
     .then(function () {
