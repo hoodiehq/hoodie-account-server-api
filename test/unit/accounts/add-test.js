3f63dd587e8592e6f9b10235f7cf41ff209d6fb4
Gregor <gregor@martynus.net>
2016-10-24 23:10:03 -0700

    test: accounts.add with profile

--- test/unit/accounts/add-test.js
+++ test/unit/accounts/add-test.js
@@ -1,3 +1,4 @@
+var simple = require('simple-mock')
 var test = require('tap').test
 
 var addAccount = require('../../../lib/accounts/add')
@@ -26,6 +27,29 @@ test('addAccount', function (group) {
       t.is(error.status, 400)
       t.is(error.message, 'username must be set')
     })
+  })
+
+  group.test('with profile', function (t) {
+    t.plan(1)
+
+    var state = {
+      db: {
+        put: simple.stub().resolveWith({})
+      }
+    }
+    addAccount(state, {
+      username: 'foo',
+      profile: {
+        name: 'bar'
+      }
+    })
+
+    .then(function () {
+      var doc = state.db.put.lastCall.arg
+      t.deepEqual(doc.profile, {name: 'bar'})
+    })
+
+    .catch(t.catch)
   })
 
   group.end()
