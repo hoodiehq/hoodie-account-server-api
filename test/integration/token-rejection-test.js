34e43827e893e761c794c64133115e89410dcf46
Victor Kai <victorkai1516@gmail.com>
2016-11-26 16:23:37 -0800

    added integration test

---
+++ test/integration/token-rejection-test.js
@@ -0,0 +1,51 @@
+var PouchDB = require('pouchdb-core')
+  .plugin(require('pouchdb-mapreduce'))
+  .plugin(require('pouchdb-adapter-memory'))
+var test = require('tap').test
+
+var getApi = require('../../')
+
+test('token rejection', function (t) {
+  var api = getApi({
+    PouchDB: PouchDB,
+    secret: 'secret'
+  })
+
+  api.accounts.add({
+    id: 'user123',
+    username: 'foo',
+    password: 'secret'
+  })
+
+  .then(function (account) {
+    t.is(account.username, 'foo', 'creates account')
+
+    return api.account({id: 'user123'}).tokens.add({
+      id: 'secrettoken',
+      type: 'passwordreset',
+      timeout: 0,
+      createdAt: '2016-11-26T00:00:00.000Z'
+    })
+  })
+
+  .then(function (token) {
+    t.is(token.timeout, 0, 'token created with timeout 0s')
+
+    return api.sessions.add({
+      account: {
+        token: 'secrettoken'
+      }
+    })
+  })
+
+  .then(function () {
+    t.fail('sessions.add() should reject expired token')
+  })
+
+  .catch(function (error) {
+    t.is(error.name, 'Not Found')
+    t.is(error.status, 404)
+    t.is(error.message, 'Token expired')
+    t.end()
+  })
+})
