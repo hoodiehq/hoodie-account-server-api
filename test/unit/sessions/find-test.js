cfe623d4fcd249fb370b24c9e5d5a0e2ba5e72aa
Gregor <gregor@martynus.net>
2017-03-06 23:51:15 -0800

    test(unit): sessions.find()

---
+++ test/unit/sessions/find-test.js
@@ -0,0 +1,65 @@
+var simple = require('simple-mock')
+var test = require('tap').test
+
+var findSession = require('../../../lib/sessions/find')
+
+test('findSession', function (group) {
+  group.test('user not found', function (t) {
+    findSession({
+      db: {
+        get: simple.stub().rejectWith(new Error('oops'))
+      }
+    }, 'sessionid')
+
+    .then(function () {
+      t.fail('should not resolve')
+    })
+
+    .catch(function (error) {
+      t.is(error.message, 'oops')
+      t.end()
+    })
+  })
+
+  group.test('user found but session invalid', function (t) {
+    findSession({
+      secret: 'secret',
+      db: {
+        get: simple.stub().resolveWith({
+          salt: 'salt123'
+        })
+      }
+    }, 'sessionid')
+
+    .then(function () {
+      t.fail('should not resolve')
+    })
+
+    .catch(function (error) {
+      t.is(error.message, 'Session invalid')
+      t.end()
+    })
+  })
+
+  group.test('user found and session valid', function (t) {
+    simple.mock(findSession.internals, 'isValidSessionId').returnWith(true)
+    findSession({
+      secret: 'secret',
+      db: {
+        get: simple.stub().resolveWith({
+          roles: ['id:user123'],
+          salt: 'salt123'
+        })
+      }
+    }, 'sessionid')
+
+    .then(function (session) {
+      t.is(session.id, 'sessionid')
+      t.end()
+    })
+
+    .catch(t.error)
+  })
+
+  group.end()
+})
